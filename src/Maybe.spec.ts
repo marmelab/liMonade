5e5878d48a0bc3c45cb2b0e32e19f32dbf9406ce
ThieryMichel <thiery@marmelab.com>
2018-11-24 17:29:55 +0100

    better implementation

--- src/Maybe.spec.ts
+++ src/Maybe.spec.ts
@@ -1,74 +1,157 @@
-import Maybe from './Maybe';
+import { Just, Nothing } from './Maybe';
 
 describe('Maybe', () => {
-    const increment = (v: number) => v + 1;
-    const double = (v: number) => v * 2;
-    const identity = (v: any) => v;
-    const doubleIdentity = (v: number) => Maybe.of(v).map(double);
-    const incrementIdentity = (v: number) => Maybe.of(v).map(increment);
+    describe('Just', () => {
+        const increment = (v: number) => v + 1;
+        const double = (v: number) => v * 2;
+        const identity = (v: any) => v;
+        const doubleIdentity = (v: number): Just<number> =>
+            Just.of(v).map(double);
+        const incrementIdentity = (v: number) => Just.of(v).map(increment);
 
-    it('.map should compose function', () => {
-        expect(
-            Maybe.of(5)
-                .map(double)
-                .map(increment),
-        ).toEqual(Maybe.of(5).map(v => increment(double(v))));
-    });
-
-    it('.map should do nothing if applying the identity function', () => {
-        expect(Maybe.of('value').map(identity)).toEqual(Maybe.of('value'));
-    });
-
-    it('.chain should be associative', () => {
-        expect(
-            Maybe.of(5)
-                .chain(doubleIdentity)
-                .chain(incrementIdentity),
-        ).toEqual(
-            Maybe.of(5).chain(v => doubleIdentity(v).chain(incrementIdentity)),
-        );
-    });
-
-    it('.chain should follow the Right identity law', () => {
-        expect(Maybe.of(5).chain(Maybe.of)).toEqual(Maybe.of(5));
-    });
-
-    it('.chain should follow the Left identity law', () => {
-        expect(Maybe.of(5).chain(incrementIdentity)).toEqual(
-            incrementIdentity(5),
-        );
-    });
-
-    describe('Applicative Functor Law', () => {
-        it('Identity', () => {
-            expect(Maybe.of(identity).ap(Maybe.of(5))).toEqual(Maybe.of(5));
+        it('.map should compose function', () => {
+            expect(
+                Just.of(5)
+                    .map(double)
+                    .map(increment),
+            ).toEqual(Just.of(5).map(v => increment(double(v))));
         });
 
-        it('Homomorphism', () => {
-            expect(Maybe.of(double).ap(Maybe.of(5))).toEqual(
-                Maybe.of(double(5)),
+        it('.map should do nothing if applying the identity function', () => {
+            expect(Just.of('value').map(identity)).toEqual(Just.of('value'));
+        });
+
+        it('.chain should be associative', () => {
+            expect(
+                Just.of(5)
+                    .chain(doubleIdentity)
+                    .chain(incrementIdentity),
+            ).toEqual(
+                Just.of(5).chain(v =>
+                    doubleIdentity(v).chain(incrementIdentity),
+                ),
             );
         });
 
-        it('Interchange', () => {
-            expect(Maybe.of(double).ap(Maybe.of(5))).toEqual(
-                Maybe.of(5).map(double),
+        it('.chain should follow the Right identity law', () => {
+            expect(Just.of(5).chain(Just.of)).toEqual(Just.of(5));
+        });
+
+        it('.chain should follow the Left identity law', () => {
+            expect(Just.of(5).chain(incrementIdentity)).toEqual(
+                incrementIdentity(5),
             );
         });
 
-        it('composition', () => {
-            const u = Maybe.of(() => 'u');
-            const v = Maybe.of((value: string) => value + 'v');
-            const w = Maybe.of('w');
-            const compose = (f1: (v: any) => any) => (f2: (v: any) => any) => (
-                value: any,
-            ) => f1(f2(value));
-            expect(u.ap(v.ap(w))).toEqual(
-                Maybe.of(compose)
-                    .ap(u)
-                    .ap(v)
-                    .ap(w),
+        describe('Applicative Functor Law', () => {
+            it('Identity', () => {
+                expect(Just.of(identity).ap(Just.of(5))).toEqual(Just.of(5));
+            });
+
+            it('Homomorphism', () => {
+                expect(Just.of(double).ap(Just.of(5))).toEqual(
+                    Just.of(double(5)),
+                );
+            });
+
+            it('Interchange', () => {
+                expect(Just.of(double).ap(Just.of(5))).toEqual(
+                    Just.of(5).map(double),
+                );
+            });
+
+            it('composition', () => {
+                const u = Just.of(() => 'u');
+                const v = Just.of((value: string) => value + 'v');
+                const w = Just.of('w');
+                const compose = (f1: (v: any) => any) => (
+                    f2: (v: any) => any,
+                ) => (value: any) => f1(f2(value));
+                expect(u.ap(v.ap(w))).toEqual(
+                    Just.of(compose)
+                        .ap(u)
+                        .ap(v)
+                        .ap(w),
+                );
+            });
+        });
+    });
+
+    describe('Nothing', () => {
+        const increment = (v: number) => v + 1;
+        const double = (v: number) => v * 2;
+        const identity = (v: any) => v;
+        const doubleIdentity = (v: number): Just<number> =>
+            Just.of(v).map(double);
+        const incrementNothing = (v: number) => Nothing.of(v).map(increment);
+
+        it('.map should compose function', () => {
+            expect(
+                Nothing.of()
+                    .map(double)
+                    .map(increment),
+            ).toEqual(Nothing.of().map(v => increment(double(v))));
+        });
+
+        it('.map should do nothing if applying the identity function', () => {
+            expect(Nothing.of().map(identity)).toEqual(Nothing.of());
+        });
+
+        it('.chain should be associative', () => {
+            expect(
+                Nothing.of()
+                    .chain(doubleIdentity)
+                    .chain(incrementNothing),
+            ).toEqual(
+                Nothing.of().chain(v =>
+                    doubleIdentity(v).chain(incrementNothing),
+                ),
             );
+        });
+
+        it('.chain should follow the Right identity law', () => {
+            expect(Nothing.of().chain(Nothing.of)).toEqual(Nothing.of());
+        });
+
+        it('.chain should follow the Left identity law', () => {
+            expect(Nothing.of().chain(incrementNothing)).toEqual(
+                incrementNothing(5),
+            );
+        });
+
+        describe('Applicative Functor Law', () => {
+            it('Identity', () => {
+                expect(Nothing.of(identity).ap(Nothing.of(5))).toEqual(
+                    Nothing.of(),
+                );
+            });
+
+            it('Homomorphism', () => {
+                expect(Nothing.of(double).ap(Nothing.of(5))).toEqual(
+                    Nothing.of(double(5)),
+                );
+            });
+
+            it('Interchange', () => {
+                expect(Nothing.of(double).ap(Nothing.of(5))).toEqual(
+                    Nothing.of(5).map(double),
+                );
+            });
+
+            it('composition', () => {
+                const u = Nothing.of(() => 'u');
+                const v = Nothing.of((value: string) => value + 'v');
+                const w = Nothing.of('w');
+                const compose = (f1: (v: any) => any) => (
+                    f2: (v: any) => any,
+                ) => (value: any) => f1(f2(value));
+                expect(u.ap(v.ap(w))).toEqual(
+                    Nothing.of(compose)
+                        .ap(u)
+                        .ap(v)
+                        .ap(w),
+                );
+            });
         });
     });
 });
--- src/Maybe.ts
+++ src/Maybe.ts
@@ -1,47 +1,84 @@
-const isNothing = (value: any): value is null =>
-    value === null || typeof value === 'undefined';
-
-class Maybe<T> {
-    public static some<T>(value: T) {
-        if (!value) {
-            throw Error('Provided value must not be empty');
-        }
-        return new Maybe(value);
+export class Just<T> {
+    public static of<A>(value: A): Just<A> {
+        return new Just(value);
     }
-    public static of<T>(value: T) {
-        return Maybe.some(value);
+    public readonly value: T;
+    constructor(value: T) {
+        this.value = value;
     }
-
-    public static none<T>() {
-        return new Maybe<T>(null);
+    public isNothing(): this is Just<null | undefined> {
+        return this.value === null || typeof this.value === 'undefined';
     }
-
-    public static fromValue<T>(value: T) {
-        return value ? Maybe.some(value) : Maybe.none<T>();
+    public isJust(): this is Just<{}> {
+        return this.value !== null && typeof this.value !== 'undefined';
     }
-    private constructor(private value: T | null) {}
-
-    public getOrElse(defaultValue: T) {
-        return this.value === null ? defaultValue : this.value;
+    public map<A>(this: Just<T>, fn: (v: T) => A): Just<A>;
+    public map<A>(this: Just<null | undefined>, fn: (v: T) => A): Nothing;
+    public map<A>(
+        this: Just<T> | Just<null | undefined>,
+        fn: (v: T) => A,
+    ): Just<A> | Nothing {
+        return this.isNothing() ? new Nothing() : new Just(fn(this.value));
     }
-    public map<A>(f: (wrapped: T) => A): Maybe<A> {
-        if (isNothing(this.value)) {
-            return Maybe.none<A>();
-        }
-        return Maybe.some(f(this.value));
+    public flatten(): T {
+        return this.value;
     }
-    public chain<A>(f: (wrapped: T) => Maybe<A>): Maybe<A> {
-        if (isNothing(this.value)) {
-            return Maybe.none<A>();
-        }
-        return f(this.value);
+    public chain<A>(this: Just<T>, fn: (v: T) => Just<A>): Just<A>;
+    public chain<A>(this: Just<T>, fn: (v: T) => Nothing): Nothing;
+    public chain<A>(
+        this: Just<null | undefined>,
+        fn: (v: T) => Nothing | Just<A>,
+    ): Nothing;
+    public chain<A>(
+        this: Just<T> | Just<null | undefined>,
+        fn: (v: T) => Just<A> | Nothing,
+    ): Just<A> | Nothing {
+        return this.isNothing() ? new Nothing() : this.map(fn).flatten();
     }
-    public ap<A, B>(this: Maybe<(v: A) => B>, other: Maybe<A>): Maybe<B> {
-        if (isNothing(this.value)) {
-            return Maybe.none<B>();
-        }
-        return this.chain(fn => other.map(fn));
+    public ap<A, B>(this: Just<(v: A) => B>, other: Just<A>): Just<B>;
+    public ap<A, B>(
+        this: Just<(v: A) => B> | Just<null | undefined>,
+        other: Nothing,
+    ): Nothing;
+    public ap<A, B>(this: Just<null | undefined>, other: Just<A>): Nothing;
+    public ap<A, B>(
+        this: Just<(v: A) => B> | Just<null | undefined>,
+        other: Just<A> | Nothing,
+    ): Just<B> | Nothing {
+        return this.isNothing() || !other.isJust()
+            ? new Nothing()
+            : this.map(fn => fn(other.flatten()));
+    }
+    public getOrElse<A>(this: Just<T>, value: A): T;
+    public getOrElse<A>(this: Just<null | undefined>, value: A): A;
+    public getOrElse<A>(
+        this: Just<T> | Just<null | undefined>,
+        value: A,
+    ): A | T {
+        return this.isNothing() ? value : this.value;
     }
 }
 
-export default Maybe;
+export class Nothing {
+    public static of(_?: any): Nothing {
+        return new Nothing();
+    }
+    public isNothing(): this is Just<null | undefined> {
+        return true;
+    }
+    public isJust(): this is Just<{}> {
+        return false;
+    }
+    public map({}): Nothing {
+        return this;
+    }
+    public flatten() {
+        return this;
+    }
+    public chain({}): Nothing {
+        return this;
+    }
+    public ap({}): Nothing {
+        return this;
+    }
+}
