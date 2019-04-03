465f76deb79053a5e17b97004b122c863ea784e3
ThieryMichel <thiery@marmelab.com>
2019-04-03 11:37:57 +0200

    add property based testing for monad law

--- src/testUtils/testMonadLaw.ts
+++ src/testUtils/testMonadLaw.ts
@@ -1,40 +1,64 @@
+import * as fc from 'fast-check';
 import { InferCategory } from '../types';
+import numberOperation from './numberOperation';
 
 export const testMonadLaw = <Name>(
     Testee: InferCategory<number, Name>,
     getValue: (v: any) => any = v => v,
 ) => {
-    const increment = (v: number) => v + 1;
-    const double = (v: number) => v * 2;
-    const doubleList = (v: number) => Testee.of(v).map(double);
-    const incrementList = (v: number) => Testee.of(v).map(increment);
-
     describe('Monad Laws', () => {
         it('.chain should be associative', async () => {
-            expect(
-                await getValue(
-                    Testee.of(5)
-                        .chain(doubleList)
-                        .chain(incrementList),
-                ),
-            ).toEqual(
-                await getValue(
-                    Testee.of(5).chain((v: number) =>
-                        doubleList(v).chain(incrementList),
-                    ),
+            fc.assert(
+                fc.asyncProperty(
+                    fc.integer(),
+                    fc.integer(),
+                    fc.integer(),
+                    numberOperation,
+                    numberOperation,
+                    async (x, y, z, fn1, fn2) => {
+                        const liftedFn1 = Testee.lift(fn1(y));
+                        const liftedFn2 = Testee.lift(fn2(z));
+                        expect(
+                            await getValue(
+                                Testee.of(x)
+                                    .chain(liftedFn1)
+                                    .chain(liftedFn2),
+                            ),
+                        ).toEqual(
+                            await getValue(
+                                Testee.of(x).chain((v: number) =>
+                                    liftedFn1(v).chain(liftedFn2),
+                                ),
+                            ),
+                        );
+                    },
                 ),
             );
         });
 
         it('.chain should follow the Right identity law', async () => {
-            expect(await getValue(Testee.of(5).chain(Testee.of))).toEqual(
-                await getValue(Testee.of(5)),
+            fc.assert(
+                fc.asyncProperty(fc.anything(), async x => {
+                    expect(
+                        await getValue(Testee.of(x).chain(Testee.of)),
+                    ).toEqual(await getValue(Testee.of(x)));
+                }),
             );
         });
 
         it('.chain should follow the Left identity law', async () => {
-            expect(await getValue(Testee.of(5).chain(incrementList))).toEqual(
-                await getValue(incrementList(5)),
+            fc.assert(
+                fc.asyncProperty(
+                    fc.integer(),
+                    fc.integer(),
+                    numberOperation,
+                    async (x, y, fn) => {
+                        const liftedFn = Testee.lift(fn(y));
+                        expect(
+                            await getValue(Testee.of(x).chain(liftedFn)),
+                        ).toEqual(await getValue(liftedFn(x)));
+                    },
+                ),
             );
         });
     });
