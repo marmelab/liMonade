28f32e5094051327c4f1a6c4126a99507e2655a4
ThieryMichel <thiery@marmelab.com>
2019-04-17 11:14:46 +0200

    uncomment code

--- src/Compose.spec.ts
+++ src/Compose.spec.ts
@@ -1,11 +1,11 @@
 import getCompose from './Compose';
 import Either from './Either';
 import Identity from './Identity';
-// import { testFunctorLaw } from './testUtils/testFunctorLaw';
 import { testApplicativeLaw } from './testUtils/testApplicativeLaw';
+import { testFunctorLaw } from './testUtils/testFunctorLaw';
 
 describe('Compose', () => {
     const Compose = getCompose(Identity, Either);
-    // testFunctorLaw(Compose);
+    testFunctorLaw(Compose);
     testApplicativeLaw(Compose);
 });
