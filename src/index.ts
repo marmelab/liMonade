257243c73983a53568c39a5339cb264fec303f00
ThieryMichel <thiery@marmelab.com>
2019-04-17 19:46:23 +0200

    add index

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import getCompose from './Compose';
+import Either, { EitherType, Left, Right } from './Either';
+import Identity, { IdentityType } from './Identity';
+import IO, { IOType } from './IO';
+import List, { ListType } from './List';
+import Maybe, { MaybeType } from './Maybe';
+import Reader, { ReaderType } from './Reader';
+import State, { StateType } from './State';
+import Task, { TaskType } from './Task';
+import Writer, { WriterType } from './Writer';
+
+export {
+    getCompose,
+    Either,
+    EitherType,
+    Left,
+    Right,
+    Identity,
+    IdentityType,
+    IO,
+    IOType,
+    List,
+    ListType,
+    Maybe,
+    MaybeType,
+    Reader,
+    ReaderType,
+    State,
+    StateType,
+    Task,
+    TaskType,
+    Writer,
+    WriterType,
+};
