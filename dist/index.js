314782ea31b67f3012031269cd2d28f1a7bbc615
ThieryMichel <thiery@marmelab.com>
2019-04-17 19:57:10 +0200

    add build and publish command

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var world = '🗺️';
-function hello(word) {
-    if (word === void 0) { word = world; }
-    return "Hello " + word + "! ";
-}
-exports.hello = hello;
-//# sourceMappingURL=index.js.map
